a141686881334ead410c8429d3b369302cc5e979
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-06-29 03:57:46 +0300

    0x01-fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-#!/usr/bin/pup
-# Docs 
-package {'flask':
+#!/usr/bin/env puppet
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
